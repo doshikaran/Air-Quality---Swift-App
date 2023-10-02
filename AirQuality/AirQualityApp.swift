41858029ad249e718a2278769743f7fb28ec1cc9
doshi karan <doshikaran005@gmail.com>
2023-10-01 20:45:12 -0700

    Initial Commit

---
+++ AirQuality/AirQualityApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirQualityApp.swift
+//  AirQuality
+//
+//  Created by Karan Doshi on 10/1/23.
+//
+
+import SwiftUI
+
+@main
+struct AirQualityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
